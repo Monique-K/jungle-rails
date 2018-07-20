4d621f993a017d0e3fb13d66e763086699c25b9c
M-Khoury <mkhoury87@hotmail.com>
2018-07-20 20:31:05 +0000

    reviews can be deleted by the user who created them

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,6 +7,16 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find params[:id]
     @review = Review.new
-  end
+
+    sum = 0
+    @product.reviews.each do |review|
+      if @product.reviews.count != 0
+        sum += review.rating
+        @average_rating = sum / @product.reviews.count
+      else
+        @average_rating = 0
+      end
+    end
+    end
 
 end
